01cdef1b39772ebf2b4b009ee15e65e71dd4947c
UT258 <utkarshsingh404499@gmail.com>
2024-06-26 21:17:58 +0530

    firebase added

--- app/src/main/java/com/example/weatherapp/view/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -16,14 +18,12 @@ import com.example.weatherapp.databinding.ActivityMainBinding;
 import com.example.weatherapp.model.WeatherResponse;
 import com.example.weatherapp.viewmodel.WeatherViewModel;
 
-
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private ActivityMainBinding binding;
     private WeatherViewModel weatherViewModel;
-    private RelativeLayout rootView;
-
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         binding.setViewModel(weatherViewModel);
         binding.setLifecycleOwner(this);
 
+        webView = binding.webview;
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new CustomWebViewClient());
 
         binding.getWeatherButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,49 +45,70 @@ public class MainActivity extends AppCompatActivity {
                 String city = binding.cityEditText.getText().toString();
                 if (!TextUtils.isEmpty(city)) {
                     weatherViewModel.fetchWeatherData(city);
+                    loadWeatherWebsite(city);
                 }
             }
         });
 
-        weatherViewModel.getWeatherData().observe(MainActivity.this, new Observer<WeatherResponse>() {
+        weatherViewModel.getWeatherData().observe(this, new Observer<WeatherResponse>() {
             @Override
             public void onChanged(WeatherResponse weatherResponse) {
                 if (weatherResponse != null) {
-
                     binding.weatherTextView.setText("City: " + weatherResponse.getCityName());
                     binding.textView.setText(weatherResponse.getMain().getTemp() + "°C");
                     binding.textView2.setText("Description: " + weatherResponse.getWeather().get(0).getDescription());
+                    setBackground(weatherResponse.getWeather().get(0).getDescription());
                 } else {
                     Log.e(TAG, "WeatherResponse is null");
                 }
-                setBackground(weatherResponse.getWeather().get(0).getDescription());
             }
         });
     }
 
     private void setBackground(String description) {
-        rootView = findViewById(R.id.rootView);
+        binding.getRoot().setBackgroundResource(getBackgroundResource(description));
+    }
 
+    private int getBackgroundResource(String description) {
         if (description.contains("clear sky")) {
-            rootView.setBackgroundResource(R.drawable.clear_sky);
+            return R.drawable.clear_sky;
         } else if (description.contains("few clouds")) {
-            rootView.setBackgroundResource(R.drawable.few_clouds);
+            return R.drawable.few_clouds;
         } else if (description.contains("scattered clouds")) {
-            rootView.setBackgroundResource(R.drawable.scattered_clouds);
+            return R.drawable.scattered_clouds;
         } else if (description.contains("broken clouds")) {
-            rootView.setBackgroundResource(R.drawable.broken_clouds);
+            return R.drawable.broken_clouds;
         } else if (description.contains("shower rain")) {
-            rootView.setBackgroundResource(R.drawable.shower_rain);
+            return R.drawable.shower_rain;
         } else if (description.contains("rain") || (description.contains("light rain"))) {
-            rootView.setBackgroundResource(R.drawable.rain);
+            return R.drawable.rain;
         } else if (description.contains("thunderstorm")) {
-            rootView.setBackgroundResource(R.drawable.thunderstorm);
+            return R.drawable.thunderstorm;
         } else if (description.contains("snow")) {
-            rootView.setBackgroundResource(R.drawable.snow);
+            return R.drawable.snow;
         } else if (description.contains("mist")) {
-            rootView.setBackgroundResource(R.drawable.mist);
+            return R.drawable.mist;
         } else {
-            rootView.setBackgroundResource(R.drawable.weatherbg);
+            return R.drawable.weatherbg;
+        }
+    }
+
+    private void loadWeatherWebsite(String city) {
+        String formattedCity = city.replaceAll("\\s+", "-"); // Replace spaces with hyphens for URL
+        String url = "https://www.theweather.com/" + formattedCity.toLowerCase().trim() + ".htm";
+        webView.setVisibility(View.VISIBLE);
+        webView.loadUrl(url);
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            view.loadUrl("javascript:(function() { " +
+                    "var elements = document.getElementsByClassName('ad'); " +
+                    "for (var i = 0; i < elements.length; i++) { " +
+                    "   elements[i].style.display='none'; " +
+                    "}})();");
         }
     }
 }
